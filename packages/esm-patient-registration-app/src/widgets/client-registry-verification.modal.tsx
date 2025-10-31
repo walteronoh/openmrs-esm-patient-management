5c185f61e934f847b132de9165d1f0bcb039e131
Walter Kiprono <walterkiprono81@gmail.com>
2025-11-01 02:05:22 +0300

    chore: add HIE and AMRS data in CR Pop up

--- packages/esm-patient-registration-app/src/widgets/client-registry-verification.modal.tsx
+++ packages/esm-patient-registration-app/src/widgets/client-registry-verification.modal.tsx
@@ -4,43 +4,22 @@ import { Button, ModalBody, ModalFooter, ModalHeader, Select, Form, TextInput, S
 
 interface ClientRegistryVerificationPropsModal {
   close(): void;
-  onConfirm(): void;
+  Component: React.FC;
+  props: object;
 }
 
-const ClientRegistryVerificationModal: React.FC<ClientRegistryVerificationPropsModal> = ({ close, onConfirm }) => {
+const ClientRegistryVerificationModal: React.FC<ClientRegistryVerificationPropsModal> = ({
+  close,
+  Component,
+  props,
+}) => {
   const { t } = useTranslation();
-  const identificationTypes = [
-    { text: 'National ID', value: '' },
-    { text: 'Refugee ID', value: '' },
-    { text: 'Alien ID', value: '' },
-    { text: 'Mandate Number', value: '' },
-  ];
-
   return (
     <>
       <ModalHeader closeModal={close} title={t('hieClientVerificationTitle', 'HIE Client Verification')} />
       <ModalBody>
-        <Form>
-          <Select>
-            {identificationTypes.map((selectItem) => (
-              <SelectItem text={selectItem.text} value={selectItem.value} />
-            ))}
-          </Select>
-          <TextInput
-            id="client-registry-id"
-            labelText="National ID or Alien ID"
-            placeholder="Enter identification number"
-          />
-        </Form>
+        <Component {...props} />
       </ModalBody>
-      <ModalFooter>
-        <Button kind="secondary" onClick={close}>
-          {t('cancel', 'Cancel')}
-        </Button>
-        <Button kind="danger" onClick={onConfirm}>
-          {t('discard', 'Discard')}
-        </Button>
-      </ModalFooter>
     </>
   );
 };
